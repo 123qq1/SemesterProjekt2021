90ef7829be45325a8588a726f38df628b1a03525
Jeppe Nedergaard Bjerremand <jeppe.bjerremand@gmail.com>
2021-11-25 10:44:18 +0100

    Simple forbindelse til DB

---
+++ DatabaseAccessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace SemesterProjekt2021
+{
+    class DatabaseAccessor
+    {
+
+        private static SqlConnection currentConnection;
+
+        public static bool ConnectToDatabase(string dbName)
+        {
+            bool succes = false;
+
+            string strconn = @"Data Source=" + Environment.MachineName +";Initial Catalog=" + dbName + ";Integrated Security=True;TrustServerCertificate=True";
+
+            //string strconn = @"Data Source=LAPTOP-1HT927JP;Initial Catalog=Semesterprojekt2021;Integrated Security=True;TrustServerCertificate=True";
+                        
+            currentConnection = new SqlConnection(strconn);
+
+            try
+            {
+                currentConnection.Open();
+                currentConnection.Close();
+
+                succes = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return succes;
+        }
+
+
+    }
+}
---
+++ TestForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemesterProjekt2021
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            bool connectionStatus = DatabaseAccessor.ConnectToDatabase("Semesterproject2021");
+
+            PingResult.Text = connectionStatus.ToString();
+
+        }
+    }
+}
