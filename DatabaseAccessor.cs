e855e532c00a21dc68867d2a1f8fb94d2a7145c1
Jeppe Nedergaard Bjerremand <jeppe.bjerremand@gmail.com>
2021-12-03 10:19:59 +0100

    Added selling setting active to false

--- DatabaseAccessor.cs
+++ DatabaseAccessor.cs
@@ -344,7 +344,7 @@ namespace SemesterProjekt2021
             {
                 sqlString = sqlString.Remove(sqlString.Length - 2);
 
-                sqlString += " WHERE ID = @BID;";
+                sqlString += " WHERE ID = @ID;";
                 MessageBox.Show(sqlString);
 
                 currentCommand.CommandText = sqlString;
@@ -676,6 +676,7 @@ namespace SemesterProjekt2021
             b.SellingPrice = sellingPrice;
             b.SoldDate = soldDate;
             b.IsSold = true;
+            b.Active = false;
 
             return UpdateBolig(b);
         }
