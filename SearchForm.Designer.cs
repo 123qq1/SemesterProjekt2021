600515d69276209a5a2fdd96c9b3066134e8f837
ChrisOtt0 <sophusott@gmail.com>
2021-12-13 12:04:18 +0100

    GUI Hotfix

--- SearchForm.Designer.cs
+++ SearchForm.Designer.cs
@@ -76,7 +76,7 @@ namespace SemesterProjekt2021
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(70, 100);
+            this.label2.Location = new System.Drawing.Point(70, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(349, 21);
             this.label2.TabIndex = 2;
@@ -85,7 +85,7 @@ namespace SemesterProjekt2021
             // PrintSome
             // 
             this.PrintSome.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.PrintSome.Location = new System.Drawing.Point(277, 137);
+            this.PrintSome.Location = new System.Drawing.Point(277, 168);
             this.PrintSome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.PrintSome.Name = "PrintSome";
             this.PrintSome.Size = new System.Drawing.Size(82, 23);
@@ -96,7 +96,7 @@ namespace SemesterProjekt2021
             // 
             // byInputTilSearch
             // 
-            this.byInputTilSearch.Location = new System.Drawing.Point(161, 137);
+            this.byInputTilSearch.Location = new System.Drawing.Point(161, 168);
             this.byInputTilSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.byInputTilSearch.Name = "byInputTilSearch";
             this.byInputTilSearch.Size = new System.Drawing.Size(110, 23);
@@ -108,7 +108,7 @@ namespace SemesterProjekt2021
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(70, 135);
+            this.label3.Location = new System.Drawing.Point(70, 166);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 21);
             this.label3.TabIndex = 7;
@@ -116,7 +116,7 @@ namespace SemesterProjekt2021
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(150, 208);
+            this.dateTimePicker1.Location = new System.Drawing.Point(150, 264);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(219, 23);
@@ -126,7 +126,7 @@ namespace SemesterProjekt2021
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(70, 171);
+            this.label4.Location = new System.Drawing.Point(70, 227);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(591, 21);
             this.label4.TabIndex = 9;
@@ -135,7 +135,7 @@ namespace SemesterProjekt2021
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(150, 249);
+            this.dateTimePicker2.Location = new System.Drawing.Point(150, 305);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(219, 23);
@@ -145,7 +145,7 @@ namespace SemesterProjekt2021
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(70, 208);
+            this.label5.Location = new System.Drawing.Point(70, 264);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 21);
             this.label5.TabIndex = 11;
@@ -155,7 +155,7 @@ namespace SemesterProjekt2021
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(70, 249);
+            this.label6.Location = new System.Drawing.Point(70, 305);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(55, 21);
             this.label6.TabIndex = 12;
@@ -163,7 +163,7 @@ namespace SemesterProjekt2021
             // 
             // printMellemDatoer
             // 
-            this.printMellemDatoer.Location = new System.Drawing.Point(338, 287);
+            this.printMellemDatoer.Location = new System.Drawing.Point(338, 343);
             this.printMellemDatoer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.printMellemDatoer.Name = "printMellemDatoer";
             this.printMellemDatoer.Size = new System.Drawing.Size(82, 23);
@@ -176,7 +176,7 @@ namespace SemesterProjekt2021
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(70, 289);
+            this.label7.Location = new System.Drawing.Point(70, 345);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(146, 21);
             this.label7.TabIndex = 14;
@@ -184,7 +184,7 @@ namespace SemesterProjekt2021
             // 
             // inputBeløbTilWeirdSearch
             // 
-            this.inputBeløbTilWeirdSearch.Location = new System.Drawing.Point(222, 287);
+            this.inputBeløbTilWeirdSearch.Location = new System.Drawing.Point(222, 343);
             this.inputBeløbTilWeirdSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.inputBeløbTilWeirdSearch.Name = "inputBeløbTilWeirdSearch";
             this.inputBeløbTilWeirdSearch.Size = new System.Drawing.Size(110, 23);
@@ -202,7 +202,7 @@ namespace SemesterProjekt2021
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(109)))), ((int)(((byte)(132)))));
-            this.panel3.Location = new System.Drawing.Point(333, 282);
+            this.panel3.Location = new System.Drawing.Point(333, 338);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(93, 34);
             this.panel3.TabIndex = 19;
@@ -210,7 +210,7 @@ namespace SemesterProjekt2021
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(109)))), ((int)(((byte)(132)))));
-            this.panel2.Location = new System.Drawing.Point(273, 132);
+            this.panel2.Location = new System.Drawing.Point(273, 163);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(91, 34);
             this.panel2.TabIndex = 20;
@@ -219,7 +219,7 @@ namespace SemesterProjekt2021
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(700, 385);
             this.Controls.Add(this.inputBeløbTilWeirdSearch);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.printMellemDatoer);
